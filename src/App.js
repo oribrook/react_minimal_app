119bed0245f72077d4f99878039594f9d6edca90
ori_b <ori@ori>
2023-05-18 10:48:59 +0300

    start button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import MyInfinitScroll from "./MyInfiniteScroll";
 import Notes from "./Notes";
 import NotesWithoutPagination from "./NotesWithoutPagination";
-import { axiosExample } from "./client/axios-example";
 
 function App() {
+  const [start, setStart] = useState(false);
   return (
     <div>
-      <div>
+      {!start && (
+        <div>
           <h1> Logicode pagination tutorial project </h1>
-          <br/>
-          <h5> This app should work together with BE from the following git repo: </h5>
+          <br />
+          <h5>
+            This app should work together with BE from the following git repo:
+          </h5>
           <h5> https://github.com/oribrook/minimalApp.git </h5>
           <h5> Branch: pagination </h5>
-          <br/>
-          <h5> After setting the BE, remove those lines from App component, and uncomment the 'Notes' component </h5>
-      </div>
-      <NotesWithoutPagination/>
-      <MyInfinitScroll/>
-      <button onClick={axiosExample}> Axios example </button>
-      <Notes/>      
+          <br />
+          <h5> After setting the BE, press the start button </h5>
+          <button
+            onClick={() => {
+              setStart(true);
+            }}
+          >            
+            Start
+          </button>
+        </div>
+      )}
+
+      {start && <div>
+        <NotesWithoutPagination />
+        <MyInfinitScroll />
+        <Notes />
+      </div>}
+
     </div>
   );
 }
