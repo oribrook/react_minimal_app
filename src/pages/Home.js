88b1bae0cf22c1f6db35e610446a0ae4353942af
ori_b <ori@ori>
2023-08-29 11:02:44 +0300

    adding docs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,26 @@ import React from 'react'
 function Home() {
   return (
     <div style={{textAlign: 'center', fontSize: "50px"}}>
-      דף הבית</div>
+      <h2> לוגיקוד </h2>
+      <h5>
+        פרוייקט זה נועד להמחשת תהליך התחברות\הרשמה של ריאקט 
+        <br />
+        כדי שהפרוייקט יעבוד כראוי, עליך לדאוג ששרת ג'אנגו ירוץ לוקאלית
+        <br />
+        <br />
+        השתמש בגיט הבא בשביל להריץ שרת ג'אנגו מותאם לפרוייקט זה
+        <br />
+        <br />
+        https://github.com/oribrook/minimalApp.git
+        <br />
+        <br />
+        branch:  token_auth
+
+
+      </h5>
+
+    
+    </div>
   )
 }
 
